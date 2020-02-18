5fcf8d98c5873b86a7773018b0de1db0138460be
GossipDolphin <mmollevik@hotmail.com>
2020-02-18 16:51:56 +0100

    made methodes for converting game money(chips) from real money. and vise versa

--- src/model/Interfaces/ICurrency.java
+++ src/model/Interfaces/ICurrency.java
@@ -1,4 +1,8 @@
 package model.Interfaces;
 
+import model.Currencies.Currency;
+
 public interface ICurrency {
+    public float gameCurrencyToRealCurrency(float amount);
+    public float RealCurrencyToGameCurrency(float amount);
 }
--- src/model/RealMoney.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-import model.Interfaces.ICurrency;
-
-public class RealMoney implements ICurrency {
-}
