2d0b120f14d3a3bc543f67eca90e3476a61921fb
GossipDolphin <mmollevik@hotmail.com>
2020-02-25 15:36:48 +0100

    fixed small bugg inn chip generator

--- src/model/Currencies/Chip.java
+++ src/model/Currencies/Chip.java
@@ -7,24 +7,35 @@ public class Chip extends Currency {
         super(builder);
     }
 
+    /**
+     *
+     * @param currency takes a currency object and uses the amount and rate from the object to generate a list with chips
+     * @return
+     */
     public static ArrayList<Chip> genreateStandardRateChipSetFromRealCurrency(Currency currency){
         double amountChipValue = currency.getAmount() * currency.getRate();
         ArrayList<Chip> ListWithChips = new ArrayList<>();
-        for (int i = 0; i < amountChipValue; i+=5){
+        int i = 0;
+        while (i < amountChipValue){
             if (i < 100){
                 ListWithChips.add(new Chip(new CurrencyBuilder().value(5).color("white")));
+                i += 5;
             }
             else if (i < 300){
                 ListWithChips.add(new Chip(new CurrencyBuilder().value(10).color("Green")));
-            }
-            else if (i < 500){
-                ListWithChips.add(new Chip(new CurrencyBuilder().value(20).color("Red")));
+                i+=10;
             }
             else if (i < 1000){
+                ListWithChips.add(new Chip(new CurrencyBuilder().value(20).color("Red")));
+                i+=20;
+            }
+            else if (i < 3000){
                 ListWithChips.add(new Chip(new CurrencyBuilder().value(50).color("Blue")));
+                i +=50;
             }
             else {
                 ListWithChips.add(new Chip(new CurrencyBuilder().value(100).color("Black")));
+                i += 100;
             }
         }
         return ListWithChips;
